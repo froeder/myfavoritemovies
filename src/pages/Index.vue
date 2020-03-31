bfdb3a86379d3ee7d93ded0d6f59e8aa9d4ac84a
Jhonatan Froeder <froeder3@gmail.com>
2020-03-30 22:48:57 -0400

    alteracoes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,63 @@
 <template>
-  <q-page class="flex flex-center">
-    <div>Your App here  </div>
+  <q-page>
+    <div class="row q-pa-lg q-gutter-md flex flex-center">
+      <div class="col-3" v-for="m in movies" v-bind:key="m.id">
+        <q-card>
+          <q-img
+            src="https://cdn.quasar.dev/img/parallax2.jpg"
+            basic
+          >
+            <div class="absolute-bottom text-h6">
+              {{m.title}}
+            </div>
+          </q-img>
+
+          <q-card-section>
+            {{m.overview}}
+          </q-card-section>
+          <q-card-section>
+            {{m.vote_average}}
+          </q-card-section>
+          <q-card-section>
+            <q-expansion-item
+              v-model="false"
+              icon="perm_identity"
+              label="Account settings"
+              caption="John Doe"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import Axios from 'axios'
 export default {
-  name: 'PageIndex'
+  name: 'HomeScreen',
+  data() {
+    return {
+      movies: new Object() 
+    }
+  } ,
+  methods : {
+
+  } , 
+  mounted(){
+    let self = this
+    Axios.get("https://api.themoviedb.org/3/list/137674?api_key=446f26dec5122c9a80566828521adc36&language=pt-BR")
+    .then(response => {
+      self.movies = response.data.items
+    })
+  }
 }
 </script>
