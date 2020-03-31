b6336ce602e6a117282c18efb33c91e3beb2c058
Jhonatan Froeder <froeder3@gmail.com>
2020-03-30 22:53:46 -0400

    build

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -18,22 +18,6 @@
           <q-card-section>
             {{m.vote_average}}
           </q-card-section>
-          <q-card-section>
-            <q-expansion-item
-              v-model="false"
-              icon="perm_identity"
-              label="Account settings"
-              caption="John Doe"
-            >
-              <q-card>
-                <q-card-section>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                  eveniet doloribus ullam aliquid.
-                </q-card-section>
-              </q-card>
-            </q-expansion-item>
-          </q-card-section>
         </q-card>
       </div>
     </div>
